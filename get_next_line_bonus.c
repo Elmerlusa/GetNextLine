b37f03f0f19f49438942c1d91f5baa71a026c88f
javmarti <martingallegojavier@outlook.com>
2022-11-06 16:44:27 +0100

    program size fixed

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -18,18 +18,22 @@ void	update_buffer(char *buffer);
 
 char	*get_next_line(int fd)
 {
-	static char	buffer[1024][BUFFER_SIZE + 1];
+	static char	*buffer[1024];
 	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
 	line = (char *)ft_calloc(1, sizeof(char));
-	line = join_line(line, buffer[fd]);
+	if (buffer[fd] == NULL)
+		buffer[fd] = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	else
+		line = join_line(line, buffer[fd]);
 	line = read_buffer(fd, buffer[fd], line);
-	if (line[0] == '\0' || line == NULL || read(fd, NULL, 0) < 0)
+	if (line == NULL || *line == '\0')
 	{
 		free(line);
-		buffer[fd][0] = '\0';
+		free(buffer[fd]);
+		buffer[fd] = NULL;
 		return (NULL);
 	}
 	update_buffer(buffer[fd]);
@@ -42,8 +46,11 @@ char	*join_line(char *line, char *buffer)
 	char	*aux;
 	char	*aux_line;
 
-	if (line == NULL || buffer == NULL)
+	if (buffer == NULL || line == NULL)
+	{
+		free(line);
 		return (NULL);
+	}
 	aux = ft_strchr(buffer, '\n');
 	if (aux == NULL)
 		new_line = ft_strjoin(line, buffer);
@@ -62,13 +69,21 @@ char	*read_buffer(int fd, char *buffer, char *line)
 	int		bytes_read;
 
 	if (line == NULL || buffer == NULL)
+	{
+		free(line);
 		return (NULL);
+	}
 	if (ft_strchr(line, '\n') != NULL)
 		return (line);
 	bytes_read = 1;
 	while (bytes_read > 0 && ft_strchr(buffer, '\n') == NULL)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read < 0)
+		{
+			free(line);
+			return (NULL);
+		}
 		buffer[bytes_read] = '\0';
 		line = join_line(line, buffer);
 		if (line == NULL)

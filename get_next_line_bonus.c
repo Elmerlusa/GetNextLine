ddd171cd5b00d58177cfc568855631dc43761082
javmarti <martingallegojavier@outlook.com>
2022-11-11 14:03:04 +0100

    check buffsize

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*join_line(char *line, char *buffer);
 char	*read_buffer(int fd, char *buffer, char *line);
